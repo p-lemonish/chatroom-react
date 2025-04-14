43987073a756cdf404e402b3fc864a0d85153d1a
Lemonish <patrikmihelson@gmail.com>
2025-04-14 22:25:37 +0300

    added react router dom for routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,24 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
-import App from './App.tsx';
+import FrontPage from './FrontPage.tsx';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import MainPage from './MainPage.tsx';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <FrontPage />
+    },
+    {
+        path: "/main",
+        element: <MainPage />
+    },
+]);
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <CssBaseline />
-        <App />
+        <RouterProvider router={router} />
     </StrictMode>,
 );
