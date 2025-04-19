bfdb69ffe10ebf2966c72f208b101c9eb5ddf8e7
Lemonish <patrikmihelson@gmail.com>
2025-04-19 21:56:02 +0300

    redoing Mainpage to work as the central hub for joining rooms

--- src/MainPage.tsx
+++ src/MainPage.tsx
@@ -5,70 +5,22 @@ import useWebSocket, { ReadyState } from "react-use-websocket";
 import { WebSocketMessage } from "react-use-websocket/dist/lib/types";
 
 function MainPage() {
-
-    // TODOs
-    // - current contents of this component to be reused as component for chatrooms generally
-    // - mainpage for joining/creating chats, there should always be the main chat avaialble by default
-    // - sidepanel for navigating/creating chatrooms
-    // - ui work, would like to see the chat interface looking more like a terminal? for example the TextField
-    //      for building the message could look like ->  username > messagehere, kind of oldschool look?
-
-    const socketUrl = 'ws://localhost:8080/main';
-    const {
-        sendJsonMessage,
-        lastMessage,
-        readyState,
-    } = useWebSocket(socketUrl, {
-        //Will attempt to reconnect on all close events, such as server shutting down
-        shouldReconnect: () => true,
-        onOpen: (() => {
-            sendJsonMessage({
-                type: "auth",
-                username: username + "_supersecretmessage", // this could be a request to the backend to send a jwt as well.
-                text: "hello server",
-            });
-        }),
-    });
     const location = useLocation();
     const username = location.state?.username;
-    const [messageHistory, setMessageHistory] = useState<MessageEvent<any>[]>([]);
-    const [message, setMessage] = useState<WebSocketMessage>("");
-    const messageEndRef = useRef<null | HTMLDivElement>(null);
-    const MAXMESSAGESIZE = 200;
+    const [rooms, setRooms] = useState<string[]>([]);
+    const roonmsUrl = "http://localhost:8080/rooms";
 
-    useEffect(() => {
-        if (lastMessage !== null) {
-            setMessageHistory((prev: any) => [lastMessage, ...prev.slice(0, MAXMESSAGESIZE)]);
+    const getAvailableRooms = async () => {
+        try {
+            const response = await fetch(roonmsUrl);
+            const json = await response.json();
+            if (response.ok) {
+                setRooms(json.rooms);
+            }
+        } catch (err) {
+            console.error(err);
         }
-    }, [lastMessage]);
-
-    useEffect(() => {
-        const scrollToBottom = () => {
-            messageEndRef.current?.scrollIntoView({ behavior: "instant", block: "end" });
-        };
-        scrollToBottom();
-    }, [messageHistory]);
-
-    const handleClickSendMessage = useCallback(() => {
-        sendJsonMessage({
-            type: "message",
-            username: username,
-            text: message,
-        });
-        setMessage("");
-    }, [message]);
-
-    const connectionStatus = {
-        [ReadyState.CONNECTING]: 'Connecting',
-        [ReadyState.OPEN]: 'Open',
-        [ReadyState.CLOSING]: 'Closing',
-        [ReadyState.CLOSED]: 'Closed',
-        [ReadyState.UNINSTANTIATED]: 'Uninstantiated',
-    }[readyState];
-
-    function handleMessageChange(event: any) {
-        setMessage(event.target.value);
-    }
+    };
 
     return (
         <Container
@@ -80,29 +32,39 @@ function MainPage() {
                 paddingBottom: '20px',
                 paddingTop: '20px',
             }}>
-            <Typography variant="h4">
-                Welcome to the Main chat (Connection status: {connectionStatus})
+            <Typography variant="h3">
+                Welcome to the Main page, {username}.
+            </Typography>
+            <Typography variant="h5">
+                Choose a room to join or create your own!
             </Typography>
             <Box
                 height={'100vh'}
                 justifyContent={'flex-end'}
                 alignItems={'flex-start'}
                 flexDirection={'column'}
-                sx={{ overflowY: 'scroll' }}
+                sx={{ overflowY: 'auto' }}
             >
-                {messageHistory.map((message: any, idx: any) => (
-                    <Typography key={idx}>
-                        {message ? message.data : null}
-                    </Typography>
-                ))}
-                <div ref={messageEndRef} />
+                {rooms.length !== 0 ?
+                    (
+                        rooms.map((roomName: any, idx: any) => (
+                            <Typography key={idx}>
+                                {roomName ? (
+                                    roomName.data + " join"
+                                ) : (
+                                    "Unnamed room"
+                                )}
+                            </Typography>
+                        ))
+                    ) : (
+                        <Typography>
+                            No rooms yet, create one!
+                        </Typography>
+                    )
+                }
             </Box>
-            <TextField variant='outlined' value={message} onChange={handleMessageChange}>
-            </TextField>
-            <Button variant='contained' onClick={handleClickSendMessage}>
-                Chat!
-            </Button>
-        </Container >
+
+        </Container>
     );
 }
 export default MainPage;
