e4f5b383808e4a3e5b628af300999a8b39720f46
Lemonish <patrikmihelson@gmail.com>
2025-05-16 23:48:33 +0300

    cleaned vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 5173,
-        proxy: {
-
-            '/api': {
-                target: 'http://localhost:8080',
-                changeOrigin: true,
-                secure: false,
-                rewrite: path => path.replace(/^\/api/, ''),
-            },
-
-            '/ws': {
-                target: 'ws://localhost:8080',
-                ws: true,
-                changeOrigin: true,
-                rewrite: path => path.replace(/^\/ws/, '/chat'),
-            },
-        },
-    },
+  plugins: [react()],
 });
-
